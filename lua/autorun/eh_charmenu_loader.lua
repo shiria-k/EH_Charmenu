2980b909385231587c32f1238f5ef2c47b5f1d6e
Shira <270121865+shiria-k@users.noreply.github.com>
2026-06-20 11:59:58 +0200

    Load Workshop resource downloader

--- lua/autorun/eh_charmenu_loader.lua
+++ lua/autorun/eh_charmenu_loader.lua
@@ -9,6 +9,7 @@ end
 includeShared("eh_charmenu/sh_config.lua")
 
 if SERVER then
+    include("eh_charmenu/sv_resources.lua")
     include("eh_charmenu/sv_database.lua")
     include("eh_charmenu/sv_core.lua")
     AddCSLuaFile("eh_charmenu/cl_menu.lua")
